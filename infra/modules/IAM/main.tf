f11333cfc73acaf2bfb60d730c6abb3d228affa9
Justin Stendara <Justin.Stendara@coxautoinc.com>
2023-11-02 19:58:06 -0500

    TF changes

--- infra/modules/IAM/main.tf
+++ infra/modules/IAM/main.tf
@@ -1,23 +1,22 @@
 resource "aws_iam_user" "minecraft-server" {
   name = "${var.app-name}-iam-user"
-  path = "/system/"
+  path = "/IAM/user/"
 }
 
 resource "aws_iam_access_key" "minecraft-server" {
   user    = aws_iam_user.minecraft-server.name
-  pgp_key = "keybase:minecraft-server"
 }
 
 data "aws_iam_policy_document" "minecraft-server" {
   statement {
     effect    = "Allow"
-    actions   = ["ec2:Describe*"]
+    actions   = ["ec2:*"]
     resources = ["*"]
   }
 }
 
 resource "aws_iam_user_policy" "minecraft-server" {
-  name   = "test"
+  name   = "test_iam_user"
   user   = aws_iam_user.minecraft-server.name
   policy = data.aws_iam_policy_document.minecraft-server.json
 }
