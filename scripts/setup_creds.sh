c9f90bb84f4731a03f25334deed53b791471ea97
Justin Stendara <Justin.Stendara@coxautoinc.com>
2023-11-16 10:27:31 -0600

    House keeping

--- scripts/setup_creds.sh
+++ scripts/setup_creds.sh
@@ -1,39 +1,48 @@
 export instance_id=i-03485a5bde9eaa3bd
 
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
+export AWS_SESSION_TOKEN
+export AWS_CREDS
+
 while IFS=, read -r col_1 col_2
 do
   echo "Setting AWS_ACCESS_KEY_ID to: $col_1"
-  export AWS_ACCESS_KEY_ID=$col_1
+  AWS_ACCESS_KEY_ID=$col_1
   echo "Setting AWS_SECRET_ACCESS_KEY to: [****${col_2:${#col_2}-4:4}]"
-  export AWS_SECRET_ACCESS_KEY=$col_2
+  AWS_SECRET_ACCESS_KEY=$col_2
 done < "../secrets/minecraft-server-iam-user.csv"
 
-export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
-export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 
-export stuff=$(aws sts get-session-token --duration-seconds 3600 --output json)
+AWS_CREDS=$(aws sts get-session-token --duration-seconds 3600 --output json)
 
-export AWS_ACCESS_KEY_ID=$(echo "${stuff}" |jq -r ".Credentials.AccessKeyId")
-export AWS_SECRET_ACCESS_KEY=$(echo "${stuff}" |jq -r ".Credentials.SecretAccessKey")
-export AWS_SESSION_TOKEN=$(echo "${stuff}" |jq -r ".Credentials.SessionToken")
+AWS_ACCESS_KEY_ID=$(echo "${AWS_CREDS}" |jq -r ".Credentials.AccessKeyId")
+AWS_SECRET_ACCESS_KEY=$(echo "${AWS_CREDS}" |jq -r ".Credentials.SecretAccessKey")
+AWS_SESSION_TOKEN=$(echo "${AWS_CREDS}" |jq -r ".Credentials.SessionToken")
 
 read -p "Would you like to start or stop the server?" operation
+
+export startupStatus
+export waitForStartup
+export IPAddress
 
 if [ "$operation" = "start" ]; then
   echo "Starting the server"
   aws ec2 start-instances --instance-ids $instance_id
 
-  export startupStatus=$(aws ec2 describe-instance-status --instance-ids "$instance_id" --output json)
-  export waitForStartup=$(echo $startupStatus | jq -r ".InstanceStatuses[0].InstanceState.Name")
+  startupStatus=$(aws ec2 describe-instance-status --instance-ids "$instance_id" --output json)
+  waitForStartup=$(echo $startupStatus | jq -r ".InstanceStatuses[0].InstanceState.Name")
   while [ "$waitForStartup" != "running" ]
   do
     echo "Checking the current status of the server..."
-    export startupStatus=$(aws ec2 describe-instance-status --instance-ids "$instance_id" --output json)
-    export waitForStartup=$(echo $startupStatus | jq -r ".InstanceStatuses[0].InstanceState.Name")
+    startupStatus=$(aws ec2 describe-instance-status --instance-ids "$instance_id" --output json)
+    waitForStartup=$(echo $startupStatus | jq -r ".InstanceStatuses[0].InstanceState.Name")
   done
 
   echo "The EC2 instance is running, sleeping for 40 seconds to allow it to start up properly..."
-  export IPAddress=$(aws ec2 describe-instances --instance-ids "$instance_id" --output json | jq -r '.Reservations[].Instances[].PublicIpAddress')
+  IPAddress=$(aws ec2 describe-instances --instance-ids "$instance_id" --output json | jq -r '.Reservations[].Instances[].PublicIpAddress')
   sleep 40 | pv -t
 
   echo "The server has been successfully started. Starting the Minecraft server."
