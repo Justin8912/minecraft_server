eb839e05118257b3f0a4b2d4b514a127d7bb5c93
Justin Stendara <Justin.Stendara@coxautoinc.com>
2024-06-21 11:08:09 -0500

    Got the lambda code working

--- lambda-code/startServer.py
+++ lambda-code/startServer.py
@@ -2,9 +2,22 @@ import boto3
 import os
 
 def lambda_handler(event, context):
-    print("Hello world")
     instance_id = os.environ["instance_id"]
     ec2 = boto3.client('ec2')
-    instance_id = 'Your-Instance-ID'
-    ec2.start_instances(InstanceIds=[instance_id])
-    print('Started EC2 instance: ' + instance_id)
+    action = event.get("action").lower()
+    if (action == "start"):
+        print("Starting EC2 instance...")
+        try:
+            ec2.start_instances(InstanceIds=[instance_id])
+        except (error):
+            print("There was an error when attempting to start the server:\n ", error)
+        print('Started EC2 instance: ' + instance_id)
+    elif (action == "stop"):
+        print("Stopping EC2 instance...")
+        try:
+            ec2.stop_instances(InstanceIds=[instance_id])
+        except (error):
+            print("There was an error when attempting to stop the server:\n ", error)
+        print('Stopped EC2 instance: ' + instance_id)
+    else:
+        print("Action was not identified, please try again using the following formatting:\n{\"action\":\"start/stop\"}.")
